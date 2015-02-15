7cea13749d726a9ee711026ffdad6fd26495913e
wjeon <novajeon@yahoo.com>
2015-02-15 03:00:40 -0800

    TFH-002 Added the first unit test for the TinyFakeHost

--- TinyFakeHostHelper.Tests.Unit/TinyFakeHostTests.cs
+++ TinyFakeHostHelper.Tests.Unit/TinyFakeHostTests.cs
@@ -1,6 +1,15 @@
+﻿using NUnit.Framework;
+
 namespace TinyFakeHostHelper.Tests.Unit
 {
+    [TestFixture]
     public class TinyFakeHostTests
     {
+        [Test]
+        public void When_construct_TinyFakeHost_it_is_constructed_with_host_uri()
+        {
+            const string uri = "http://localhost:5432/someService/v1/";
+            Assert.DoesNotThrow(() => new TinyFakeHost(uri));
+        }
     }
 }
