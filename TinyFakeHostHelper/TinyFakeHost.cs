e5708d31b0ef62ec3797340c0ed3bd22097a0291
wjeon <novajeon@yahoo.com>
2015-02-15 12:52:58 -0800

    TFH-002 Implemented the TinyFakeHost with Nancy.Hosting.Self to pass the integration test

--- TinyFakeHostHelper/TinyFakeHost.cs
+++ TinyFakeHostHelper/TinyFakeHost.cs
@@ -1,23 +1,32 @@
 ﻿using System;
+using Nancy.Hosting.Self;
 
 namespace TinyFakeHostHelper
 {
     public class TinyFakeHost : IDisposable
     {
+        private readonly NancyHost _nancyHost;
+
         public TinyFakeHost(string uri)
         {
+            var nancyHostConfig = new HostConfiguration { UrlReservations = { CreateAutomatically = true } };
+
+            _nancyHost = new NancyHost(nancyHostConfig, new Uri(uri.TrimEnd('/') + "/"));
         }
 
         public void Start()
         {
+            _nancyHost.Start();
         }
 
         public void Stop()
         {
+            _nancyHost.Stop();
         }
 
         public void Dispose()
         {
+            _nancyHost.Dispose();
         }
     }
 }
