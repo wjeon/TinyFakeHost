85755c2bd1d53407b46b992f2a5bb9460ab3d121
wjeon <novajeon@yahoo.com>
2015-03-03 01:19:34 -0800

    TFH-005 Added FakeRequestResponse

---
+++ TinyFakeHostHelper/RequestResponse/FakeRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace TinyFakeHostHelper.RequestResponse
+{
+    public class FakeRequest
+    {
+        public FakeRequest()
+        {
+            Headers = new Dictionary<string, string>();
+        }
+
+        public IDictionary<string, string> Headers { get; set; }
+        public string Path { get; set; }
+        public string Parameters { get; set; }
+    }
+}
---
+++ TinyFakeHostHelper/RequestResponse/FakeResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace TinyFakeHostHelper.RequestResponse
+{
+    public class FakeResponse
+    {
+        public FakeResponse()
+        {
+            Headers = new Dictionary<string, string>();
+            StatusCode = System.Net.HttpStatusCode.OK;
+        }
+
+        public string ContentType { get; set; }
+        public string Content { get; set; }
+        public IDictionary<string, string> Headers { get; set; }
+        public string ReasonPhrase { get; set; }
+        public System.Net.HttpStatusCode StatusCode { get; set; }
+    }
+}
