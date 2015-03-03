d9a93e16622af8348d9ada7e27057d209157b821
wjeon <novajeon@yahoo.com>
2015-03-03 00:47:09 -0800

    TFH-004 Added TryStart logic in Start method

---
+++ TinyFakeHostHelper/Extensions/GuidExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TinyFakeHostHelper.Tests
+{
+    public static class GuidExtensions
+    {
+        public static string FirstSegment(this Guid guid)
+        {
+            var data = guid.ToString().Split('-');
+            return data[0];
+        }
+    }
+}
