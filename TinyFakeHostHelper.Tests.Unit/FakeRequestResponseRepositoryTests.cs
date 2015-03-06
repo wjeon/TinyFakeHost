5ca2d71900e868635f69f1a104cb9650801962e5
wjeon <novajeon@yahoo.com>
2015-03-05 23:29:03 -0800

    TFH-006 Added FakeRequestResponseRepository

---
+++ TinyFakeHostHelper.Tests.Unit/FakeRequestResponseRepositoryTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using NUnit.Framework;
+using TinyFakeHostHelper.Persistence;
+using TinyFakeHostHelper.RequestResponse;
+
+namespace TinyFakeHostHelper.Tests.Unit
+{
+    [TestFixture]
+    public class FakeRequestResponseRepositoryTests
+    {
+        [Test]
+        public void It_adds_FakeRequestResponse_and_reads_all_added_FakeRequestResponses()
+        {
+            var fakeRequestResponseRepository = new FakeRequestResponseRepository();
+
+            var firstFakeRequestResponse = CreateFakeRequestResponseWith("A", HttpStatusCode.OK);
+            var secondFakeRequestResponse = CreateFakeRequestResponseWith("B", HttpStatusCode.BadRequest);
+
+            fakeRequestResponseRepository.Add(firstFakeRequestResponse);
+            fakeRequestResponseRepository.Add(secondFakeRequestResponse);
+
+            var fakeRequestResponses = fakeRequestResponseRepository.GetAll();
+
+            var expectedFakeRequestResponses = new List<FakeRequestResponse>
+            {
+                firstFakeRequestResponse, secondFakeRequestResponse
+            };
+
+            Assert.IsTrue(fakeRequestResponses.SequenceEqual(expectedFakeRequestResponses));
+        }
+
+        private static FakeRequestResponse CreateFakeRequestResponseWith(string key, HttpStatusCode statusCode)
+        {
+            var fakeRequestResponse = new FakeRequestResponse
+            {
+                FakeRequest = new FakeRequest
+                {
+                    Headers = new Dictionary<string, string> { { "header-" + key, "value-" + key } },
+                    Path = "/" + key + "/products",
+                    Parameters = new UrlParameters(new List<UrlParameter>
+                    {
+                        new UrlParameter("param-" + key + "-1", "value-" + key + "-1"),
+                        new UrlParameter("param-" + key + "-2", "value-" + key + "-2")
+                    })
+                },
+                FakeResponse = new FakeResponse
+                {
+                    Content = "content " + key,
+                    ContentType = "application/json",
+                    Headers = new Dictionary<string, string> {{"header-" + key, "value-" + key}},
+                    ReasonPhrase = "reason phrase " + key,
+                    StatusCode = statusCode
+                }
+            };
+
+            return fakeRequestResponse;
+        }
+    }
+}
