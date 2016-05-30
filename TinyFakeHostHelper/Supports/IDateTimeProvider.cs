3c152fba8281c5969657129248d55c36af7450af
wjeon <novajeon@yahoo.com>
2016-05-29 17:50:11 -0700

    TFH-022 Added DateTimeProvider and registered it

---
+++ TinyFakeHostHelper/Supports/IDateTimeProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TinyFakeHostHelper.Supports
+{
+    public interface IDateTimeProvider
+    {
+        DateTimeOffset Now { get; }
+        DateTimeOffset UtcNow { get; }
+    }
+}
