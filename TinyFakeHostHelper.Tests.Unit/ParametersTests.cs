10e7f7ee044c78561c2a275e5f5d152fef91d5bd
Woo jun Jeon <wjeon@paypoint.com>
2016-06-05 22:03:06 -0700

    TFH-021 Refactored ParametersTests and added a new test for unhappy path

--- TinyFakeHostHelper.Tests.Unit/ParametersTests.cs
+++ TinyFakeHostHelper.Tests.Unit/ParametersTests.cs
@@ -8,16 +8,31 @@ namespace TinyFakeHostHelper.Tests.Unit
     [TestFixture]
     public class ParametersTests
     {
-        [Test]
-        public void When_Parameters_and_dynamic_dictionary_for_requested_parameters_are_equal_Equals_method_returns_true()
+        private Parameters _parameters;
+
+        [SetUp]
+        public void SetUp()
         {
             var parameters = new List<Parameter> { new Parameter("KeyA", "ValueA"), new Parameter("KeyB", "ValueB") };
 
-            var urlParameters = new Parameters(parameters);
+            _parameters = new Parameters(parameters);
+        }
 
+        [Test]
+        public void When_Parameters_and_dynamic_dictionary_for_requested_parameters_are_equal_Equals_method_returns_true()
+        {
             var requestedParameters = new DynamicDictionary { {"KeyB", "ValueB"}, {"KeyA", "ValueA"} };
 
-            Assert.IsTrue(urlParameters.Equals(requestedParameters));
+            Assert.IsTrue(_parameters.Equals(requestedParameters));
+        }
+
+        [Test]
+        public void When_Parameters_and_dynamic_dictionary_for_requested_parameters_are_not_equal_Equals_method_returns_false()
+        {
+            var requestedParameters = new DynamicDictionary { { "KeyB", "ValueA" }, { "KeyA", "ValueB" } };
+
+            Assert.IsFalse(_parameters.Equals(requestedParameters));
+        }
         }
     }
 }
